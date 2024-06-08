b1d29129a5022025343ce0a62018cf8aa780a0be
Soumyadeep Kundu <soumyadeep.30505@gmail.com>
2024-06-08 23:44:23 +0530

    Code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Technologies from "./components/Technologies";
 
 export default function App() {
   return (
-    <div className="overflow-x-hidden antialiased text-neutral-200 selection:bg-gold-100 selection:text-purple-800 no-scrollbar">
+    <div className="overflow-x-hidden antialiased text-neutral-200 selection:bg-gold-100 selection:text-purple-800">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       </div>
